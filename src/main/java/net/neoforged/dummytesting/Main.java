ca7728903aee869a71db2755fd1feacb326aaf40
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2023-12-26 21:29:12 +0200

    Update Main.java

--- src/main/java/net/neoforged/dummytesting/Main.java
+++ src/main/java/net/neoforged/dummytesting/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sum: " + add(Stream.of(args).mapToInt(Integer::valueOf).toArray()));
+        System.out.println("It's actually that, trust me bro");
     }
 
     public static int add(int... values) {
