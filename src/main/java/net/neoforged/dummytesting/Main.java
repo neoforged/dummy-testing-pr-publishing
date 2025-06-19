3a5dc66d42bbd612b76eddd8537b1c87a63e6163
Matyrobbrt <matyrobbrt@gmail.com>
2025-06-19 16:27:16 +0300

    Update Main.java

--- src/main/java/net/neoforged/dummytesting/Main.java
+++ src/main/java/net/neoforged/dummytesting/Main.java
@@ -6,6 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sum: " + add(Stream.of(args).mapToInt(Integer::valueOf).toArray()));
+
+        System.out.println("::error title=GameTest Failure::1 required game tests failed: neotests:tests/alwaysfail");
+        System.out.println("::error title=GameTest Failure::1 required game tests failed: neotests:tests/alwaysfail");
+        System.out.println("::error title=GameTest Failure::1 required game tests failed: neotests:tests/alwaysfail");
     }
 
     public static <T> void someVarArgs(T... ts) {
